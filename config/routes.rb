f0d2cd3494c5fb4de3f0fefb8d0a3d476edf3246
joellusky <joelzlusky@gmail.com>
2015-05-12 17:08:27 -0400

    after_sign_in_path_for redirects to phrases_path

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
+	devise_scope :user do
+  	devise_for :users
+  	root to: 'devise/sessions#create'
+  end
+  
   resources :phrases, only: [:index, :new] do
     resources :phrase_collabs, only: [:new]
   end
 
-  root to: 'phrases#index'
+  
 end
