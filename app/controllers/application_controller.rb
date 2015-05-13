bee8a1db11256101f900f269cec8e08abd31a322
joellusky <joelzlusky@gmail.com>
2015-05-13 10:31:12 -0400

    added sign in layout and made layout dynamic

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,15 @@
 class ApplicationController < ActionController::Base
+	layout :layout_by_resource
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  before_action :authenticate_user!
 
   def after_sign_in_path_for(resources)
   	phrases_path
+  end
+
+  def layout_by_resource
+  	devise_controller? ? "sign_in" : "application"
   end
 end
